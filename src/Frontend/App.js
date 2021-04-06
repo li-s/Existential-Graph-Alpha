d66fbf7330b3e5b233b161c76a09884ed8dfe858
li-s <lishuo980@gmail.com>
2021-04-06 21:23:25 +0800

    Add way to select graphs

--- src/Frontend/App.js
+++ src/Frontend/App.js
@@ -1,33 +1,39 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 import Submission from './Submission'
 import Graph from './Graph'
 import parseString from '../Logic/Parser'
-import {convert_to_tree} from '../Logic/Tree'
+import { convert_to_tree } from '../Logic/Tree'
 
 class App extends Component {
   state = {
-    inputFormula : '',
-    parsedFormula : '',
-    tree : null,
-    selectedFormula : null,
+    inputFormula: '',
+    parsedFormula: '',
+    tree: null,
+    selectedFormula: null,
+    selectedFormulaTraversal: null,
   }
 
-  handleSubmit = (form) => { 
-    const{formula} = form
-    this.setState({inputFormula: formula})
-    this.setState({parsedFormula : parseString(formula)})
-    this.setState({tree : convert_to_tree(parseString(formula))})
+  handleSubmit = (form) => {
+    const { formula } = form
+    this.setState({ inputFormula: formula })
+    this.setState({ parsedFormula: parseString(formula) })
+    this.setState({ tree: convert_to_tree(parseString(formula)) })
   }
 
-  render () {
-    const{inputFormula, parsedFormula, tree, selectedFormula} = this.state
+  handleSelect = ({ tree, traversal }) => {
+    this.setState({ selectedFormula: tree })
+    this.setState({ selectedFormulaTraversal: traversal })
+  }
+
+  render() {
+    const { inputFormula, parsedFormula, tree, selectedFormula } = this.state
 
     return (
-      <div className="rootPage">        
+      <div className="rootPage">
         <p>Your input: {inputFormula}</p>
         <p>Your formula: {parsedFormula}</p>
-        <Graph tree = {tree} />
+        <Graph tree={tree} handleSelect={this.handleSelect} traversal={[]} />
         <Submission handleSubmit={this.handleSubmit} />
       </div>
     )
